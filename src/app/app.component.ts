24e9e36e449c888f0db59f485082053cfbf40816
viktorcerepanov <cherepanov0077@gmail.com>
2025-04-16 18:15:41 +0500

    Created header, event-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {EventCardComponent} from './common-ui/event-card/event-card.component';
+import {HeaderComponent} from './common-ui/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EventCardComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
