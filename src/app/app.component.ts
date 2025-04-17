76c2665e3f05c0f3f7d90fcf6e8a5aadcdb47773
viktorcerepanov <cherepanov0077@gmail.com>
2025-04-18 01:39:57 +0500

    Created search-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EventCardComponent} from './common-ui/event-card/event-card.component';
 import {HeaderComponent} from './common-ui/header/header.component';
+import {SearchBarComponent} from './common-ui/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EventCardComponent, HeaderComponent],
+  imports: [RouterOutlet, EventCardComponent, HeaderComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
