817027509979ffc58bccc3120935528d53867ee0
DESKTOP-1ADU77N\Константин <obukhovkostya@gmail.com>
2025-04-18 16:59:01 +0500

    event-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EventCardComponent} from './common-ui/event-card/event-card.component';
-import {HeaderComponent} from './common-ui/header/header.component';
+import { HeaderComponent } from './common-ui/header/header.component';
+import { EventPageComponent } from './pages/event-page/event-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EventCardComponent, HeaderComponent],
+  imports: [RouterOutlet, EventCardComponent, HeaderComponent, EventPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
