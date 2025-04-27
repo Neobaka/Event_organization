6826d0b7311be53d7d241db437983247e3a11e4a
viktorcerepanov <cherepanov0077@gmail.com>
2025-04-27 17:29:52 +0500

    create-user-profile-page: commit 1

--- src/app/pages/user-profile-page/user-profile-page.component.ts
+++ src/app/pages/user-profile-page/user-profile-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '../../common-ui/header/header.component';
+import {UserProfileDataBlockComponent} from '../../layout/user-profile-data-block/user-profile-data-block.component';
 
 @Component({
   selector: 'app-user-profile-page',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    UserProfileDataBlockComponent
+  ],
   templateUrl: './user-profile-page.component.html',
   styleUrl: './user-profile-page.component.scss'
 })
