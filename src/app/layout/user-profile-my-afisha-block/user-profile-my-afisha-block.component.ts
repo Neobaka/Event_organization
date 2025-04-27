5adb40a86399893a26686b8a3022cb678448fc6d
viktorcerepanov <cherepanov0077@gmail.com>
2025-04-27 20:36:04 +0500

    create-user-profile-page: commit 2

---
+++ src/app/layout/user-profile-my-afisha-block/user-profile-my-afisha-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {SearchBarComponent} from '../../common-ui/search-bar/search-bar.component';
+
+@Component({
+  selector: 'app-user-profile-my-afisha-block',
+  imports: [],
+  templateUrl: './user-profile-my-afisha-block.component.html',
+  styleUrl: './user-profile-my-afisha-block.component.scss'
+})
+export class UserProfileMyAfishaBlockComponent {
+
+}
