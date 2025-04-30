8286ab1ab80ed91bb13b7fb058c799bf4a604dc8
viktorcerepanov <cherepanov0077@gmail.com>
2025-04-30 22:27:20 +0500

    create-user-profile-page: commit 5 minor fix

--- src/app/layout/user-profile-data-block/user-profile-data-block.component.ts
+++ src/app/layout/user-profile-data-block/user-profile-data-block.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-user-profile-data-block',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './user-profile-data-block.component.html',
   styleUrl: './user-profile-data-block.component.scss'
 })
--- src/app/pages/user-profile-page/user-profile-page.component.ts
+++ src/app/pages/user-profile-page/user-profile-page.component.ts
@@ -3,13 +3,15 @@ import {HeaderComponent} from '../../common-ui/header/header.component';
 import {UserProfileDataBlockComponent} from '../../layout/user-profile-data-block/user-profile-data-block.component';
 import {  UserProfileMyAfishaBlockComponent} from '../../layout/user-profile-my-afisha-block/user-profile-my-afisha-block.component';
 import {Router} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-user-profile-page',
   imports: [
     HeaderComponent,
     UserProfileDataBlockComponent,
-    UserProfileMyAfishaBlockComponent
+    UserProfileMyAfishaBlockComponent,
+    NgOptimizedImage
   ],
   templateUrl: './user-profile-page.component.html',
   styleUrl: './user-profile-page.component.scss'
