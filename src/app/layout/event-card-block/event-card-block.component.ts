2f980688b124e5d5d952837d0e6d0f069fc79633
viktorcerepanov <cherepanov0077@gmail.com>
2025-05-09 01:16:33 +0500

    minor fix ui 4 fix header, mini-cards

--- src/app/layout/event-card-block/event-card-block.component.ts
+++ src/app/layout/event-card-block/event-card-block.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {EventCardComponent} from '../../common-ui/event-card/event-card.component';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-event-card-block',
   imports: [
-    EventCardComponent
+    EventCardComponent,
+    ScrollingModule
   ],
   templateUrl: './event-card-block.component.html',
   styleUrl: './event-card-block.component.scss'
