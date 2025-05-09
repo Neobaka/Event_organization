d14e62eed4918f3b56c530d8563d0086a2e7b863
DESKTOP-1ADU77N\Константин <obukhovkostya@gmail.com>
2025-05-09 19:40:28 +0500

    Revert "patch fro user-profile"
    This reverts commit 50cd410c49db44c56f07ac4b4206198d49598454.

--- src/app/layout/user-profile-data-block/user-profile-data-block.component.ts
+++ src/app/layout/user-profile-data-block/user-profile-data-block.component.ts
@@ -1,12 +1,5 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Auth2Service } from '../../auth/auth2.service';
-import firebase from 'firebase/compat/app';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-
-
-//Поскольку используем compat API, везде, где есть ссылака на User, нужно использовать тип из firebase/compat/app
-type User = firebase.User;
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-user-profile-data-block',
@@ -16,15 +9,6 @@ type User = firebase.User;
   templateUrl: './user-profile-data-block.component.html',
   styleUrl: './user-profile-data-block.component.scss'
 })
-export class UserProfileDataBlockComponent implements OnInit {
-  user: User | null = null;
-
-  constructor(private authService: Auth2Service) { }
-
-  ngOnInit(): void {
-    this.authService.user$.subscribe(user => {
-      this.user = user;
-    });
-  }
+export class UserProfileDataBlockComponent {
 
 }
